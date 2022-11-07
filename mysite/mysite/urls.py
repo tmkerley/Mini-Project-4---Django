9ea36a5381a2e4f982078de8b9b806389f68f3aa
tmkerley <70177694+tmkerley@users.noreply.github.com>
2022-11-06 21:52:44 -0700

    Playing with files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from . import views
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('', views.HomeView.as_view(), name='Home'),
 ]

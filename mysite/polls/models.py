4d82b0415650ac3c6b7a5193f7ede76e5d1aac4a
tmkerley <70177694+tmkerley@users.noreply.github.com>
2022-11-11 12:30:11 -0700

    Bootstrap fun

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,8 +7,10 @@ from django.contrib import admin
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
+        
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
